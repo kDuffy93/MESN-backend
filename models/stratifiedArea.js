fdfd71d7bddfce5f3d6d39dbdfb49aa3db9b5006
Kyle Duffy <kyleduffy83@gmail.com>
2023-03-26 00:46:44 -0400

    added model files

---
+++ models/stratifiedArea.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const schemaDefinition = {};
+
+let schemaObj = new mongoose.Schema(schemaDefinition);
+
+module.exports = mongoose.model("rentalData", schemaObj);
